489aa532d54d1f1b04d6f2d9a97a582cb3e88a38
Austin Wehn <austin.wehn@wehn.devs>
2024-03-15 14:02:58 -0700

    added recipe api's

---
+++ app/recipe/urls.py
@@ -0,0 +1,22 @@
+"""
+URL mappings for the recipe app
+"""
+from django.urls import (
+    path,
+    include
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+# assigns all of the endpoints for our recipe
+# view set to this endpoint
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
