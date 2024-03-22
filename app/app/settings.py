428b439b341bc2c1d513ca9b6e7fcf590751ccac
Austin Wehn <austin.wehn@wehn.devs>
2024-03-22 13:52:32 -0700

    making project deployment ready

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_w1=8mq7q)mk%%4sx1%2ow175z@dof_xt19&i71!^o1&ndg2gm'
+# retrieve value for key SECRET_KEY, defaulting to 'changeme' if not exists
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
